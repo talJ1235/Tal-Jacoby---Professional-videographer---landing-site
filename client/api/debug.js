0c99ed8a9c9e9f0db6cd6bf1bd97babddf18711e
Tal1235 <tal.jacoby10@gmail.com>
2026-05-03 23:39:31 +0300

    Security: remove debug endpoint

--- client/api/debug.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  res.json({
-    hasResendKey:     !!process.env.RESEND_API_KEY,
-    hasDatabase:      !!process.env.DATABASE_URL,
-    hasAdminSecret:   !!process.env.ADMIN_SECRET,
-    notificationEmail: process.env.NOTIFICATION_EMAIL || 'NOT SET',
-    resendFrom:       process.env.RESEND_FROM        || 'NOT SET',
-  });
-}
